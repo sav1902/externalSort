768f73c92b253543cb92aa943e4b4681bc5282ef
Andrey.Shilov <andrey.shilov@billing.ru>
2017-12-27 16:28:42 +0500

    maven-compiler-plugin

--- src/test/java/org/sav/externalsort/ExternalSortTest.java
+++ src/test/java/org/sav/externalsort/ExternalSortTest.java
@@ -35,6 +35,7 @@ public class ExternalSortTest {
 
     @Test
     public void defaultSorterTest() throws IOException {
+        System.out.println("defaultSorterTest");
         Path resultFilePath = Paths.get(sourceFilePath.toString() + ".defaultSorterTest.sorted");
 
         Date start = new Date();
@@ -50,6 +51,7 @@ public class ExternalSortTest {
 
     @Test
     public void mergeSorterTest() throws IOException {
+        System.out.println("mergeSorterTest");
         Path resultFilePath = Paths.get(sourceFilePath.toString() + ".mergeSorterTest.sorted");
 
         Date start = new Date();
@@ -65,6 +67,7 @@ public class ExternalSortTest {
 
     @Test
     public void googleSortTest() throws IOException {
+        System.out.println("googleSortTest");
         Path resultFilePath = Paths.get(sourceFilePath.toString() + ".googleSortTest.sorted");
         Date start = new Date();
         System.out.println("Started: " +dateFormat.format(start));
